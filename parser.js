594e046ac92016884d705472382fa7b47af0cb55
Matsi <movitelli@gmail.com>
2019-07-15 21:42:18 -0300

    Player parser

--- parser.js
+++ parser.js
@@ -1,24 +1,24 @@
 var fs = require('fs');
 
-fs.readFile(process.cwd() + "//internos-limpios.csv", function (err, data) {
+fs.readFile(process.cwd() + "//Player.csv", function (err, data) {
   if (err) throw err;
 
-  var values = data.toString().split('\n')
+  var values = data.toString().split('\r\n');
   //The first row contains the keys so i have to remove it 
-  var keys = values.shift().split(';')
-  var keysLength = keys.length
-  values = values.map(function (value) {
-    splited = value.split(';')
-    let json = {}
+  var keys = values.shift().split(',');
+  var keysLength = keys.length;
+  values = values.map((value) =>{
+    splited = value.split(',');
+    let json = {};
     for (let i = 0; i < keysLength; i++) {
-      json[keys[i]] = splited[i]
+      json[keys[i].trim()] = splited[i];
     }
-    return json
+    return json;
   });
 
   let json = JSON.stringify(values);
 
-  fs.writeFile('internos.json', json, (err) => {
+  fs.writeFile('Players.json', json, (err) => {
     if (err) throw err;
     console.log('The json file has been saved');
   });
